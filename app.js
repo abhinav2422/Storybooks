4e1a64eb8390a824b8e51b1788ebc3564c50f59d
Abhinav Jain <jabhi678@gmail.com>
2019-01-27 14:38:18 +0530

    first

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ app.get('/', (req, res) => {
     res.send('sadf');
 });
 
-const port = 5000 | process.env.PORT;
+const port = 5000 || process.env.PORT;
 app.listen(port, () => {
     console.log(`App started on port ${port}`);
 });
