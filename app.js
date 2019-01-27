8592b8e03fa5397e549b88a8560770690df48cf8
Abhinav Jain <jabhi678@gmail.com>
2019-01-27 13:15:35 +0530

    first

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('sadf');
+});
+
+const port = 5000 | process.env.PORT;
+app.listen(port, () => {
+    console.log(`App started on port ${port}`);
+});
