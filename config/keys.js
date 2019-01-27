7fcc8ee5f8ee7abc654cdc97da007734751cbc09
Abhinav Jain <jabhi678@gmail.com>
2019-01-27 16:21:47 +0530

    creds

---
+++ config/keys.js
@@ -0,0 +1,5 @@
+module.exports = {
+    mongoURI: 'mongodb://abhinav2422:Knightsandhawk9@ds213665.mlab.com:13665/storybooks-dev',
+    googleClientID: '1065668638129-pigeai38g88tjr4ggh0cfqcqe6926dfk.apps.googleusercontent.com',
+    googleClientSecret: 'ACv5oHCGoTxVG_FJufnbNA2W'
+}
