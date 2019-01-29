8ee24083e083ff0bd62170d2ec3b0c7e42358312
Abhinav Jain <jabhi678@gmail.com>
2019-01-29 15:15:15 +0530

    oauth update

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Storybooks
+
+An application developed using node.js used to store story ideas and uses google oauth.
--- app.js
+++ app.js
@@ -1,11 +1,21 @@
 const express = require('express');
+const passport = require('passport');
 const mongoose = require('mongoose');
+
+//Passport config
+require('./config/passport')(passport);
+
+//Load routes
+const auth = require('./routes/auth');
 
 const app = express();
 
 app.get('/', (req, res) => {
     res.send('sadf');
 });
+
+//Use Routes
+app.use('/auth', auth);
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
