ea47f0c603931992cd9147a3df4db9c4d4d3365b
Abhinav Jain <jabhi678@gmail.com>
2019-02-04 19:38:12 +0530

    authenticated

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const router = express.Router();
+const {ensureAuthenticated, ensureGuest} = require('../helpers/auth');
 
-router.get('/', (req, res) => {
+router.get('/', ensureGuest, (req, res) => {
     res.render('index/welcome');
 });
 
-router.get('/dashboard', (req, res) => {
+router.get('/dashboard', ensureAuthenticated, (req, res) => {
     res.render('index/dashboard');
 });
 
--- routes/stories.js
+++ routes/stories.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const {ensureAuthenticated, ensureGuest} = require('../helpers/auth');
 
 //Stories index
 router.get('/', (req, res) => {
@@ -7,7 +8,7 @@ router.get('/', (req, res) => {
 });
 
 //Add story form
-router.get('/add', (req, res) => {
+router.get('/add', ensureAuthenticated, (req, res) => {
     res.render('stories/add');
 });
 
