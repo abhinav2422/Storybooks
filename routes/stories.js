058ae31be0f1a9d632e03dc906147fcd6b63aba0
Abhinav Jain <jabhi678@gmail.com>
2019-02-05 21:18:15 +0530

    display public stories

--- routes/stories.js
+++ routes/stories.js
@@ -7,7 +7,13 @@ const {ensureAuthenticated, ensureGuest} = require('../helpers/auth');
 
 //Stories index
 router.get('/', (req, res) => {
-    res.render('stories/index');
+    Story.find({status: 'public'})
+        .populate('user')
+        .then(stories => {
+            res.render('stories/index', {
+                stories: stories
+            });
+        });
 });
 
 //Add story form
