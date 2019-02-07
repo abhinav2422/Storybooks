af09e3248abe4d3b1a29c3d4b8deb3c5e2b4c776
Abhinav Jain <jabhi678@gmail.com>
2019-02-07 15:06:22 +0530

    comment save

--- routes/stories.js
+++ routes/stories.js
@@ -101,10 +101,31 @@ router.put('/:id', (req, res) => {
 
 //Delete Story
 router.delete('/:id', (req, res) => {
-    Story.deleteOne({_id: req.params.id})
+    Story.deleteOne({ _id: req.params.id })
         .then(() => {
             res.redirect('/dashboard');
         })
+});
+
+// Add Comment
+router.post('/comment/:id', (req, res) => {
+    Story.findOne({
+        _id: req.params.id
+    })
+        .then(story => {
+            const newComment = {
+                commentBody: req.body.commentBody,
+                commentUser: req.user.id
+            }
+
+            // Add to comments array
+            story.comments.unshift(newComment);
+
+            story.save()
+                .then(story => {
+                    res.redirect(`/stories/show/${story.id}`);
+                });
+        });
 });
 
 module.exports = router;
